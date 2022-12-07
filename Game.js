d138176e5ad2051334747696cf8f1c2bca5fbafb
Yoan Laurain <80961060+Yoan-laurain@users.noreply.github.com>
2022-12-07 21:11:59 +0100

    safe delete lines

--- Game.js
+++ Game.js
@@ -1,45 +1,74 @@
-title = "";
-description = ``;
+title = "Color shoot";
+description = `Play`;
 characters = [];
-options = {};
+options = {
+    isReplayEnabled: false,
+    isDrawingScoreFront: true,
+    theme: "dark",
+    isPlayingBgm: true,
+    seed: 9,
+};
 
 class MyBlock
 {
-    constructor(struct, color,width)
+    constructor(id,struct, color,width,etage,height)
     {
         this.struct = struct;
         this.color = color;
         this.width = width;
+        this.id = id;
+        this.etage = etage;
+        this.height = height;
     }   
 }
 
 /** @type {Vector[]} */
 let pins;
-const cordLength = 20;
 
 // Shoot
 var goRight = false;
 var inputFired = false;
 var angleShooted = 0;
+var currentAngle = 0;
 
 // Blocks structure
 var pin;
-var Etages = [[],[],[],[],[]];
+var Etages = [];
 var tabColor = ["red","blue","green"];
-var widthBlock = 24;
 var heightBlock = 5;
 
 //Block movements
-var DistanceToAdd = 0.5;
 var descente = 0.02;
+var spaceBetweenBlocks = 0.5;
+var totalSpaceToFill = 98;
+
+var speeds = [];
+var nbEtage = 4;
+var nbBlockPerEtage = 5;
+var gameOver = false;
+var hitColor = null;
+
+var id =0;
+
+var shootFailed;
+var linesPop = 0;
+var multiplier = 1;
+var penalty = 1;
+
 
 function update() 
 {
     if (!ticks) 
     {
       pins = [vec(50,95)];
-      cord = { angle: 0, length: cordLength, pin: pins[0] };
+      for ( i = 0; i < nbEtage; i++ )
+      {
+        Etages[i] = [];
+        speeds[i] = 0;
+      }
     }
+
+    if ( gameOver ) return;
 
     RotationTir();
 
@@ -48,12 +77,23 @@ function update()
       box(p, 3);
     });
 
-    onKeyPress();
+    // if(nbEtage < 5)
+    // {
+    //     nbEtage += rndi(1,3);
+        
+    //     for (var i = Etages.length ; i <= nbEtage; i++)
+    //     {
+    //         Etages[i] = [];
+    //         speeds[i] = 0;
+    //     }
+    // }
 
-    CreateEtage(1);
-    CreateEtage(2);
-    CreateEtage(3);
-    CreateEtage(4);
+    for ( p = 0; p < nbEtage; p++ )
+    {
+        CreateEtage(p);
+    }
+
+    onKeyPress();
 }
 
 function CreateEtage(numEtage)
@@ -62,40 +102,53 @@ function CreateEtage(numEtage)
     if( Etages[numEtage].length == 0 )
     {
         //Position
-        var x = widthBlock / 2;
-        var y = numEtage * 6 + 10; 
+        var x;
+        var y =  numEtage * (heightBlock+1) + 10; 
+
+        var spaceLeftTofill = totalSpaceToFill;
 
         // Spawn the block
-        for ( i = 0; i < 5; i++ )
+        for ( i = 0; i < nbBlockPerEtage; i++ )
         {
+            var potentialWidth = setupWidthForBlocks(i,spaceLeftTofill,numEtage);
+            spaceLeftTofill -= potentialWidth;
+    
             if ( i != 0 )
-                x = Etages[numEtage][i-1].struct.x + widthBlock + 1;
-            if ( i == 4)
-                x =  -  widthBlock / 2 - 1;
+                x = Etages[numEtage][i-1].struct.x +  Etages[numEtage][i-1].width /2 + potentialWidth / 2 + spaceBetweenBlocks;
+            if ( i == nbBlockPerEtage - 1)
+                x =  -potentialWidth / 2 - spaceBetweenBlocks;
+            if ( i == 0)
+                x = potentialWidth / 2 ;
           
-            SpawnBlock(i,numEtage,x,y);
+            SpawnBlock(i,numEtage,x,y,potentialWidth);
+        }
+
+        if( speeds[numEtage] == 0 )
+        {
+            speeds[numEtage] = rnd(0.4,0.6);
         }
     }
     else
     {
-        // Move all 5 blocks
-        for ( i = 0; i < 5; i++ )
+        // Move all blocks
+        for ( i = 0; i < nbBlockPerEtage; i++ )
         {
             // Movements
-            Etages[numEtage][i].struct.x += DistanceToAdd;
-            Etages[numEtage][i].struct.y += descente;
+            Etages[numEtage][i].struct.x += speeds[numEtage];
+            Etages[numEtage][i].struct.y += descente * penalty;
 
             // if a block toutch the bottom, stop the game
-            if ( Etages[numEtage][i].struct.y > 98 )
+            if ( Etages[numEtage][i].struct.y > 80 )
             {
+                play("explosion");
                 end();
             }
 
             // if a block is out of the screen, replace it at the beginning
-            if( Etages[numEtage][i].struct.x >= 99 + widthBlock / 2 )
+            if( Etages[numEtage][i].struct.x >= 99 + Etages[numEtage][i].width / 2 )
             {
-                //diff =  ElementCloseToLeftSide(numEtage);
-                Etages[numEtage][i].struct.x = -widthBlock /2 - 2 ;
+                var width = Etages[numEtage][( i+1 == nbBlockPerEtage ? 0 : i+1 )].struct.x - Etages[numEtage][( i+1 == nbBlockPerEtage ? 0 : i+1 )].width / 2;
+                Etages[numEtage][i].struct.x = -abs(width)  - spaceBetweenBlocks - Etages[numEtage][i].width / 2;
             }
 
             // Place the block
@@ -104,47 +157,96 @@ function CreateEtage(numEtage)
     }
 }
 
-function SpawnBlock(indice,numEtage,x,y)
+function SpawnBlock(indice,numEtage,x,y,width)
 {
-    block = new MyBlock(vec( x, y ), tabColor[rndi(0, 3)],widthBlock);
+    block = new MyBlock(id,vec( x, y ), tabColor[rndi(0, 3)],width,numEtage,heightBlock);
+    id++;
     MoveBlock(block);
     Etages[numEtage][indice] = block;
 }
 
-function MoveBlock(block)
+function MoveBlockFired(block)
 {
-    color(block.color);
-    box(block.struct, widthBlock,heightBlock);
-    color("black");
+    var blocksInWay = GetAllBlockInWay(block);
+
+
+    if ( blocksInWay.length > 0 && !shootFailed )
+    {
+        if ( hitColor == null)
+        {
+            hitColor = blocksInWay[0].color;
+        }
+
+        play("coin");
+      
+        for ( var s = 0; s < blocksInWay.length; s++ )
+        {
+            if( blocksInWay[s].color == hitColor )
+            {
+                Etages.pop();
+                nbEtage--;   
+                linesPop++;     
+                multiplier += 1;  
+            }
+            else
+            {
+                addScore(linesPop * 100 * multiplier, 50,50);
+                hitColor = null;
+                shootFailed = true;
+                linesPopAtOnce = 0;
+                multiplier = 0;
+                penalty = 1;        
+                console      
+                break;
+            }
+
+        }
+        penalty = clamp(penalty * (3 / multiplier), 1, 4);
+    }
+    else
+    {
+        penalty = 1;      
+        color(block.color);
+        block.struct.y -= 0.5;
+        box(block.struct, block.width,block.height);
+        color("black");
+    }
+
 }
 
 function RotationTir()
 {
-    cord.angle += goRight ? -0.01 : 0.01;
+    currentAngle += goRight ? -0.02 : 0.02;
 
-    if (cord.angle > PI)
+    if (currentAngle > PI)
     {
         goRight = true;
     }
-    else if (cord.angle < 0)
+    else if (currentAngle < 0)
     {
         goRight = false;
     }
-    line(cord.pin, vec(cord.pin).addWithAngle( -cord.angle , cord.length));
+    
+    pos = vec(50, 95);
+    const np = vec(pos).addWithAngle(-currentAngle, 15);
+    color("red");
+    line(pos, np, 3);
+    color("black");    
 }
 
 function onKeyPress()
 {
-    if (input.isJustPressed && !inputFired)
-    {
-        inputFired = true;
-        angleShooted = cord.angle;
-    }
-
     if ( inputFired )
     {
-        // create a new pin
-        pin = vec(pins[pins.length - 1]).addWithAngle( -angleShooted , 1);
+        var block = new MyBlock();
+        block.struct =  vec( pins[pins.length - 1].struct.x,pins[pins.length - 1].struct.y ).addWithAngle( -angleShooted , 1);
+        block.color = pins[pins.length - 1].color;
+        block.width = 2;
+        block.id = -1;
+        block.etage = -1;
+        block.height = 2;
+
+        MoveBlockFired(block);
 
         if( pins.length > 1)
         {
@@ -152,30 +254,83 @@ function onKeyPress()
         }
 
         // Add new pin
-        pins.push(pin);
+        pins.push(block);
 
         // if the pin is out of the screen, stop shooting
-        if ( pin.x < 0 || pin.x > 99 || pin.y < 0 || pin.y > 99 )
+        if ( block.struct.x < 0 || block.struct.x > 99 || block.struct.y < 0 || block.struct.y > 99 )
         {
             inputFired = false;
+            shootFailed = false;
             pins.pop();
-        }
+        }        
     }
+
+    if (input.isJustPressed && !inputFired)
+    {
+        play("laser");
+        inputFired = true;
+        angleShooted = currentAngle;
+        block = new MyBlock(-1,vec( 50,95 ).addWithAngle( -angleShooted , 0.05), "yellow",2,-1,2);
+        MoveBlock(block);
+        pins.push(block);
+    }    
 }
 
-function ElementCloseToLeftSide(numEtage)
+function setupWidthForBlocks(i,spaceLeftTofill,numEtage)
 {
-    x = 99;
-    var block = Etages[numEtage][0];
-    for ( i = 0; i < 5; i++ )
+    basicWidth = totalSpaceToFill / nbBlockPerEtage;
+    var potentialWidth = rndi(basicWidth-5,basicWidth+5);
+
+    if(potentialWidth % 2 != 0 )
     {
-        if ( Etages[numEtage][i].struct.x < x )
+        potentialWidth++;
+    }
+
+    if ( i == nbBlockPerEtage-2 ) 
+    {
+        potentialWidth = spaceLeftTofill;
+    }
+    else if ( i == nbBlockPerEtage-1 )
+    {
+        potentialWidth = Etages[numEtage][i-1].width;
+    }
+    else 
+    {
+        while(spaceLeftTofill - potentialWidth < 0)
         {
-            x = Etages[numEtage][i].struct.x;
-            block = Etages[numEtage][i];
+            potentialWidth = rndi(basicWidth-5,basicWidth+5);
         }
     }
-    return block.width /2 ;
+    return potentialWidth;
 }
+
+function MoveBlock(block)
+{
+    color(block.color);
+    box(block.struct, block.width,block.height);
+    color("black");
+}
+
+function GetAllBlockInWay(block)
+{
+    var blocksInWay = [];
+
+    for ( i = nbEtage - 1 ; i >= 0; i-- )
+    {
+        Etages[i].forEach(element => {
+            if(  block.struct.y < element.struct.y + element.height /2 && block.struct.y > element.struct.y - element.height /2 )
+            {
+                if ( block.struct.x > element.struct.x - element.width /2 && block.struct.x < element.struct.x + element.width /2 )
+                {
+                    blocksInWay.push(element);
+                }
+            }
+        });
+    }
+
+
+    return blocksInWay;
+}
+
 
 addEventListener("load", onLoad);
